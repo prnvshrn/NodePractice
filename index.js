9a5694b19755592d021d1669455d48d660878a78
prnvshrn <prnvshrn@gmail.com>
2019-02-09 19:43:28 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get("/url",(req, res, next)=>{
+    res.json(['Batman','Superman','Green Lantern','Wonder Woman']);
+});
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000');
+});
+
